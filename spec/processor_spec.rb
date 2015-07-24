fa9f84822f4a1ee8cf7e5824c138a100e392cf41
Sean Miller <sean@seanmiller.ca>
2015-07-24 13:55:52 -0400

    Better handle test setup for tmp file reading and writing

--- spec/processor_spec.rb
+++ spec/processor_spec.rb
@@ -1,16 +1,22 @@
 require_relative "../lib/processor"
 
 RSpec.describe Processor, "#extract" do
-  context "test file" do
+  context "reading only" do
     it "retrieves middle bit" do
       p = Processor.new("spec/fixtures/2015.org", "*** Monday 20 July 2015", "*** Tuesday 21 July 2015")
       expect(p.extract).to eq("\nrelevant stuff\n\nFrom C. S. Lewis, /The Four Loves/, 162:\n\n    quoted stuff\n\nmore relevant stuff\n\n")
     end
+  end
+  context "reading and writing" do
+    before do
+      FileUtils.mkdir("spec/tmp")
+    end
+    after do
+      FileUtils.rm_rf("spec/tmp")
+    end
     it "puts link back in file" do
-      File.delete("spec/tmp/2015.org") if File.exist?("spec/tmp/2015.org")
       FileUtils.cp("spec/fixtures/2015.org", "spec/tmp/2015.org")
       expect(File.read("spec/fixtures/2015-with-link.org")).to eq(File.read("spec/tmp/2015.org"))
-      File.delete("spec/tmp/2015.org")
     end
   end
 end
